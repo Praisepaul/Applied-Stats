edd3f2320368dfbc71d9cbacc1f76fb01d0eee99
Praisepaul <praise20024christ@gmail.com>
2023-01-18 09:39:38 +0530

    Applied Stats Week 2

---
+++ 1.R
@@ -0,0 +1,7 @@
+A = matrix(c(1, 3, 5, 5, 7, 6),nrow=2, ncol=3,byrow=TRUE)
+print(A)
+
+a = 80
+b = 60
+sum = a+b
+print(sum)
---
+++ 2.R
@@ -0,0 +1,17 @@
+A = c(23,69,101,150,32,69,18,22,23,161)
+
+#a) Find length of the Vector
+print(paste("The length of vector is: ",length(A)))
+
+# b) mode of vector
+print(paste("The type of A is: ",mode(A)))
+
+# c) maximum and minimum elements of vector
+print(paste("Maximum element in A is: ",max(A)))
+print(paste("Maximum element in A is: ",min(A)))
+
+# d) Sum of all elements of the vector
+print(paste("Sum of all elements in A is: ",sum(A)))
+
+# Print the 2nd, 6th and 9th elements
+print(paste("Elements at the position 2,6 and 9 in A are: ",A[c(2.6,9)]))
